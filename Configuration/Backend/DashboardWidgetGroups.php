36bf5cb02cbf49f6c35037a43b1a13d0d39b8698
Dan Kleine (Dan Untenzu) <dkl@xima.de>
2025-11-05 07:42:46 +0100

    fix: 🐛 Add missing group for TYPO3 < 13
    Widgets where moved to the "content" group recently,
    which is however only available in TYPO3 13.
    This makes them unavailable in TYPO3 11 and 12,
    unless they have been added to the dashboard before.
    Create a content group by default now.

---
+++ Configuration/Backend/DashboardWidgetGroups.php
@@ -0,0 +1,8 @@
+<?php
+
+// @deprecated Provided by cms-dashboard in TYPO3 v13 and higher
+return [
+    'content' => [
+        'title' => 'LLL:EXT:xima_typo3_content_audit/Resources/Private/Language/locallang.xlf:widget_group.content',
+    ],
+];
